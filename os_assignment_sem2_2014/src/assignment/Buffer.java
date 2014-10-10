58e2d763f3463115fe33e3a027f2f48825b86471
Yue <makingitbettergo@gmail.com>
2014-10-10 21:14:55 +1300

    Completed the assignment with enhanced features

--- os_assignment_sem2_2014/src/assignment/Buffer.java
+++ os_assignment_sem2_2014/src/assignment/Buffer.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
 /**
- * Buffer class to simulate
+ * Buffer class to simulate Producer and Consumer problem
  * 
  * @author Yue Li 1251124
  *
@@ -18,25 +18,21 @@ public class Buffer {
 	// determine the length of the buffer
 	public static int[] BUFFER_ITEM = new int[5];
 	// determine the access of number of producers
-	public static Semaphore prod = new Semaphore(2);
+	public static Semaphore prod = new Semaphore(5);
 	// determine the access of number of consumers
 	public static Semaphore con = new Semaphore(0);
+	public static int BUFFER_INDEX;
 
 	public Buffer() {
 	}
 
-	public static void insert_item(int element) {
-		// boolean isInserted = false;
+	public static void insert_item(int element, int threadId) {
 		try {
 			prod.acquire();
-			for (int i = 0; i < BUFFER_ITEM.length; i++) {
-				if (BUFFER_ITEM[i] == 0) {
-					BUFFER_ITEM[i] = element;
-					// isInserted = true;
-					System.out.println("Inserted " + element + " at " + i);
-					break;
-				}
-			}
+			BUFFER_ITEM[BUFFER_INDEX] = element;
+			System.out.println("Producer #" + threadId + " inserted " + element
+					+ " at index: " + BUFFER_INDEX);
+			BUFFER_INDEX++;
 			System.out.print("[");
 			for (int i = 0; i < BUFFER_ITEM.length - 1; i++) {
 				System.out.print(BUFFER_ITEM[i] + ", ");
@@ -52,42 +48,43 @@ public class Buffer {
 	 * remove item for consumer threads. consumer class shall not acquire
 	 * resource until producer release the resource.
 	 */
-	public static int remove_item() {
+	public static int remove_item(int threadNum) {
 		int item = 0;
 		try {
 			con.acquire();
-			for (int i = 0; i < BUFFER_ITEM.length; i++) {
-				if (BUFFER_ITEM[i] != 0) {
-					item = BUFFER_ITEM[i];
-					BUFFER_ITEM[i] = 0;
-					System.out.println("removed " + item + " at " + i);
-					System.out.print("[");
-					for (int l = 0; l < BUFFER_ITEM.length - 1; l++) {
-						System.out.print(BUFFER_ITEM[l] + ", ");
-					}
-					prod.release();
-					System.out.println(BUFFER_ITEM[4] + "]");
-					return item;
-				}
+			BUFFER_INDEX--;
+			item = BUFFER_ITEM[BUFFER_INDEX];
+			BUFFER_ITEM[BUFFER_INDEX] = 0;
+			System.out.println("Consumer #" + threadNum + " removed " + item
+					+ " at " + BUFFER_INDEX);
+			System.out.print("[");
+			for (int l = 0; l < BUFFER_ITEM.length - 1; l++) {
+				System.out.print(BUFFER_ITEM[l] + ", ");
 			}
+			System.out.println(BUFFER_ITEM[4] + "]");
+			prod.release();
+			return item;
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		return 0;
 	}
 
+	/**
+	 * Create an anonymous producer thread that always insert positive integers
+	 * into the buffer array
+	 */
 	public static void createProducerThread() {
-		System.out.println("P " + THREAD_ID++);
+		System.out.println("Producer thread #" + THREAD_ID++ + " created.");
 		Thread temp = new Thread() {
 			int threadNum = THREAD_ID;
 
 			public void run() {
 				while (true) {
 					try {
-						sleep(Buffer.RAN.nextInt(5000));
-						System.out.println("Prodcuder thread " + threadNum);
-						int item = Buffer.RAN.nextInt(100) + 1;
-						Buffer.insert_item(item);
+						sleep(Buffer.RAN.nextInt(5000) + 200);
+						int item = Buffer.RAN.nextInt(1000) + 1;
+						Buffer.insert_item(item, this.threadNum);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -97,17 +94,20 @@ public class Buffer {
 		temp.start();
 	}
 
+	/**
+	 * Create an anonymous consumer thread that always remove positive integers
+	 * from the buffer array
+	 */
 	public static void createCosumerThread() {
-		System.out.println("C " + THREAD_ID++);
+		System.out.println("Consumer thread #" + THREAD_ID++ + " created.");
 		Thread temp = new Thread() {
 			int threadNum = THREAD_ID;
 
 			public void run() {
 				while (true) {
 					try {
-						sleep(Buffer.RAN.nextInt(5000));
-						System.out.println("Consumer thread " + threadNum);
-						Buffer.remove_item();
+						sleep(Buffer.RAN.nextInt(5000) + 200);
+						Buffer.remove_item(threadNum);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
@@ -187,6 +187,8 @@ public class Buffer {
 		try {
 			System.out.println("Main Thread start under sleeping");
 			Thread.sleep(duration * 1000);
+			System.out.println("Main Thread is awake."
+					+ " The simulation is terminated");
 			System.exit(0);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
